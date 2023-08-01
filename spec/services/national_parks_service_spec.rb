d8bcc77a939bec62ff53fd903a8d1cfdaa214fb7
James White <whitejaad@gmail.com>
2023-08-01 14:45:23 -0700

    added service

---
+++ spec/services/national_parks_service_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe NationalParksService do 
+    describe 'get_all_campgrounds' do 
+        it 'returns a list of all the campgrounds' do 
+            
+        end
+    end
+end
