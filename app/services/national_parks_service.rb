64a5f1aeb8182c796adb6a8f7ffe67419f0325d9
James White <whitejaad@gmail.com>
2023-08-01 15:14:55 -0700

    got the service up and tested

--- app/services/national_parks_service.rb
+++ app/services/national_parks_service.rb
@@ -1,11 +1,16 @@
-require 'faraday'
-require 'json'
-
 class NationalParksService
   BASE_URL = 'https://developer.nps.gov/api/v1/'
   API_KEY = ENV['nps_api_key']
 
   def self.find_campgrounds
+    response = conn.get('campgrounds') do |req|
+      req.headers['X-Api-Key'] = API_KEY
+    end
+
+    parse_json(response)
+  end
+
+  def self.get_all_campgrounds
     response = conn.get('campgrounds') do |req|
       req.headers['X-Api-Key'] = API_KEY
     end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,13 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'vcr'
+require 'webmock/rspec'
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/vcr_cassettes'
+  config.hook_into :webmock
+  config.default_cassette_options = { record: :new_episodes }
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
