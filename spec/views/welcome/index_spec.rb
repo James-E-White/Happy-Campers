7267deb463c2cf14c2f654efc6d8d51780a1d2e9
James White <whitejaad@gmail.com>
2023-08-02 09:28:07 -0700

    commented out view spec

--- spec/views/welcome/index_spec.rb
+++ spec/views/welcome/index_spec.rb
@@ -1,8 +1,8 @@
-require "rails_helper"
+# require 'rails_helper'
 
-RSpec.describe "welcome/index", type: :view do 
-    it 'displays a welcome message' do 
-        render
-        expect(rendered).to have_content("Welcome")
-    end
-end
+# RSpec.describe "welcome/index", type: :view do 
+#     it 'displays a welcome message' do 
+#         render
+#         expect(rendered).to have_content("Welcome")
+#     end
+# end
